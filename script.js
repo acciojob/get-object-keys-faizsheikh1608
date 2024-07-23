a5f0a8dc52a03157f4177b2dd46d6e42340e6de4
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-07-23 23:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+// Add a method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Usage example
+console.log(student.getKeys()); /
